d02ff60118b74d240f3e8e5413afabebfebba727
Zack Gottesman <zachary.s.gottesman.22@dartmouth.edu>
2025-03-22 04:41:29 -0400

    fix typo

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ let BASE_SERVER_URL;
 
 if (url === 'https://mm-pp-frontend.onrender.com') {
     BASE_SERVER_URL = 'https://mm-pp-app.onrender.com/';
-} else if (url === 'https://pikap-march-madness.onrender.com/') {
+} else if (url === 'https://pikap-march-madness.onrender.com') {
     BASE_SERVER_URL = 'https://mm-pp-app.onrender.com/pk';
 } else {
     BASE_SERVER_URL = 'http://127.0.0.1:8000';
