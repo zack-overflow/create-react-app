8f4d55fa7704307a68bb3b67486242a87d110cc7
Zack Gottesman <zachary.s.gottesman.22@dartmouth.edu>
2025-03-22 04:28:10 -0400

    fix bug with urls

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 // Base server URL
 let BASE_SERVER_URL;
 
-if (process.env.PIKAP === 'false') {
+if (process.env.PIKAP === false) {
     BASE_SERVER_URL = 'https://mm-pp-app.onrender.com/';
-} else if (process.env.PIKAP === 'true') {
+} else if (process.env.PIKAP === true) {
     BASE_SERVER_URL = 'https://mm-pp-app.onrender.com/pk';
 } else {
     BASE_SERVER_URL = 'http://127.0.0.1:8000';
